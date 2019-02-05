a49561b8b99af96ce95832440c97eda37d98526d
Aiden Gleave <aidengleave@gmail.com>
2019-02-05 18:34:38 +0000

    Added tests for this.says (5 and 15)

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -39,5 +39,11 @@ describe('fizzBuzz', function(){
           expect(fizzbuzz.says(3)).toEqual("Fizz");
         });
       });
+
+      describe('when playing, says', function() {
+        it('"Buzz" when a number is divisible by 5', function() {
+          expect(fizzbuzz.says(5)).toEqual("Buzz");
+        });
+      });
     });
   });
