1d602977e16754e5a5ed51a958ab7d0a38513db7
Aiden Gleave <aidengleave@gmail.com>
2019-02-05 15:35:18 +0000

    Wrote tests for isDivisibleBy 5 and 15

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,19 +1,37 @@
 describe('fizzBuzz', function(){
   var fizzbuzz;
+  beforeEach(function() {
+    fizzbuzz = new fizzBuzz();
+  });
   
   describe('knows when a number is', function() {
-
+    
     it('divisible by 3', function() {
-      fizzbuzz = new fizzBuzz();
       expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);  
     });
 
-    describe('knows when a numberis NOT', function() {
+    it('divisible by 5', function() {
+      expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);  
+    });
+    
+    it('divisible by 15', function() {
+    expect(fizzbuzz.isDivisibleByFifteen(15)).toBe(true);
+    });
+
+  });  
+    
+  describe('knows when a numberis NOT', function() {
+
       it('divisible by 3', function() {
-        fizzbuzz = new fizzBuzz();
         expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+      });
+
+      it('divisible by 5', function() {
+        expect(fizzbuzz.isDivisibleByFive(3)).toBe(false);
+      });
+
+      it('divisible by 15', function() {
+        expect(fizzbuzz.isDivisibleByFifteen(14)).toBe(false);
       });
     });
   });
-});
-
