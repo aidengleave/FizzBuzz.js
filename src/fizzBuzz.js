4b2b4b7c7f8d9c87d04ec525ed9002d9c2e1947f
Aiden Gleave <aidengleave@gmail.com>
2019-02-05 15:36:51 +0000

    Added isDivisibleBy functionality - passing the tests

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -3,3 +3,11 @@ var fizzBuzz = function() {};
 fizzBuzz.prototype.isDivisibleByThree = function(number) {
   return (number % 3 === 0);
 };
+
+fizzBuzz.prototype.isDivisibleByFive = function(number) {
+  return (number % 5 === 0);
+};
+
+fizzBuzz.prototype.isDivisibleByFifteen = function(number) {
+  return (number % 15 === 0);
+};
